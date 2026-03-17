2a47db992fd0bd8fdd2bcd7b9078fb7d62cab293
Nikola Matovic <nikola.matovic@ymail.com>
2026-03-17 12:13:03 +0000

    removing pizza things

--- pizza/src/main/java/ch/fhnw/pizza/UserApplication.java
+++ pizza/src/main/java/ch/fhnw/pizza/UserApplication.java
@@ -8,10 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import ch.fhnw.pizza.business.service.MenuService;
 import ch.fhnw.pizza.business.service.UserService;
 import ch.fhnw.pizza.data.domain.Location;
-import ch.fhnw.pizza.data.domain.Pizza;
+
 import ch.fhnw.pizza.data.domain.Role;
 import ch.fhnw.pizza.data.domain.User;
 import ch.fhnw.pizza.data.domain.UserStatus;
@@ -29,28 +28,15 @@ public class UserApplication {
 
     @Bean
     CommandLineRunner initData(
-            MenuService menuService,
             UserService userService,
             LocationRepository locationRepository,
             RoleRepository roleRepository) {
 
         return args -> {
-            initPizzas(menuService);
             initUsers(userService, locationRepository, roleRepository);
         };
     }
 
-    private void initPizzas(MenuService menuService) throws Exception {
-        Pizza margherita = new Pizza();
-        margherita.setPizzaName("Margherita");
-        margherita.setPizzaToppings("Tomato sauce, mozzarella, basil");
-        menuService.addPizza(margherita);
-
-        Pizza funghi = new Pizza();
-        funghi.setPizzaName("Funghi");
-        funghi.setPizzaToppings("Tomato sauce, mozzarella, mushrooms");
-        menuService.addPizza(funghi);
-    }
 
     private void initUsers(
             UserService userService,
